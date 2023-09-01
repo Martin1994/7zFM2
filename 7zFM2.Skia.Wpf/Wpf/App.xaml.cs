62f4c99ba4e1e3ba7f7a31a0f2869799e3c15c08
Martin Chloride <i@martincl2.me>
2023-09-01 00:31:06 -0700

    WPF head

---
+++ 7zFM2.Skia.Wpf/Wpf/App.xaml.cs
@@ -0,0 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using WpfApp = System.Windows.Application;
+
+namespace SevenZip.FileManager2.Skia.Wpf;
+public partial class App : WpfApp
+{
+}
